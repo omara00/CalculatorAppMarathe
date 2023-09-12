13abbf8e5f16b892084dd3c7e863281c887cf97d
omara007 <77243318+omara00@users.noreply.github.com>
2023-09-12 11:40:37 -0500

    Added delete and clear buttons; added a catch for multiple decimals

--- CalculatorAppMarathe/app/src/main/java/com/example/calculatorappmarathe/MainActivity.java
+++ CalculatorAppMarathe/app/src/main/java/com/example/calculatorappmarathe/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.calculatorappmarathe;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
 
@@ -13,6 +14,9 @@ public class MainActivity extends AppCompatActivity {
     public String currentnum = "";
 
     public String view = "";
+
+    //stores the last change made, either a number or an operation
+    public boolean numIsLastChanged = true;
 
     TextView screen;
     public ArrayList<String> numArr = new ArrayList<String>();
@@ -30,54 +34,76 @@ public class MainActivity extends AppCompatActivity {
         if(v.getId() == R.id.button0){
             currentnum += "0";
             view += "0";
+            numIsLastChanged = true;
 
         }
         else if (v.getId() == R.id.button1){
             currentnum += "1";
             view += "1";
+            numIsLastChanged = true;
+
 
         }
         else if (v.getId() == R.id.button2){
             currentnum += "2";
             view += "2";
+            numIsLastChanged = true;
+
         }
         else if(v.getId() == R.id.button3){
             currentnum += "3";
             view += "3";
+            numIsLastChanged = true;
+
         }
         else if(v.getId() == R.id.button4){
             currentnum += "4";
             view += "4";
+            numIsLastChanged = true;
+
 
         }
         else if(v.getId() == R.id.button5){
             currentnum += "5";
             view += "5";
+            numIsLastChanged = true;
+
         }
         else if(v.getId() == R.id.button6){
             currentnum += "6";
             view += "6";
+            numIsLastChanged = true;
+
         }
         else if(v.getId() == R.id.button7){
             currentnum += "7";
             view += "7";
+            numIsLastChanged = true;
+
         }
         else if(v.getId() == R.id.button8){
             currentnum += "8";
             view += "8";
+            numIsLastChanged = true;
+
         }
         else if(v.getId() == R.id.button9){
             currentnum += "9";
             view += "9";
+            numIsLastChanged = true;
+
         }
         else if(v.getId() == R.id.button_dot){
             currentnum += ".";
             view += ".";
+            numIsLastChanged = true;
         }
         else if(v.getId() == R.id.button_equals){
             numArr.add(currentnum);
             currentnum = "";
             calculate();
+            numIsLastChanged = true;
+
 
         }
         else if(v.getId() == R.id.button_divide){
@@ -85,24 +111,32 @@ public class MainActivity extends AppCompatActivity {
             currentnum = "";
             operationArr.add("/");
             view += " / ";
+            numIsLastChanged = false;
+
         }
         else if(v.getId() == R.id.button_multiply){
             numArr.add(currentnum);
             currentnum = "";
             operationArr.add("*");
-            view += "*";
+            view += " * ";
+            numIsLastChanged = false;
+
         }
         else if(v.getId() == R.id.button_add){
             numArr.add(currentnum);
             currentnum = "";
             operationArr.add("+");
-            view += "+";
+            view += " + ";
+            numIsLastChanged = false;
+
         }
         else if(v.getId() == R.id.button_subtract){
             numArr.add(currentnum);
             currentnum = "";
             operationArr.add("-");
-            view += "-";
+            view += " - ";
+            numIsLastChanged = false;
+
         }
         if(view.length() < 3){
             screen.setText(view);
@@ -119,47 +153,91 @@ public class MainActivity extends AppCompatActivity {
 
     }
     public void calculate(){
-        if(numArr.size() - operationArr.size() == 1 && !numArr.contains("")){
-            while(operationArr.contains("*") || operationArr.contains("/")){
-                if(operationArr.contains("*")){
-                    int index = operationArr.indexOf("*");
-                    double value = Double.parseDouble(numArr.get(index)) * Double.parseDouble(numArr.get(index+1));
-                    numArr.set(index, value + "");
-                    numArr.remove(index + 1);
-                    operationArr.remove(index);
-                }
-                else{
-                    int index = operationArr.indexOf("/");
-                    double value = Double.parseDouble(numArr.get(index)) / Double.parseDouble(numArr.get(index+1));
-                    numArr.set(index, value + "");
-                    numArr.remove(index + 1);
-                    operationArr.remove(index);
+        //catch error statements
+        boolean two_decimals = false;
+        for(String s: numArr){
+            int decimals = 0;
+            for(int i = 0; i < s.length(); i++){
+                if(s.charAt(i) == '.'){
+                    decimals++;
                 }
             }
-            while(operationArr.size() > 0){
-                if(operationArr.contains("+")){
-                    int index = operationArr.indexOf("+");
-                    double value = Double.parseDouble(numArr.get(index)) + Double.parseDouble(numArr.get(index+1));
-                    numArr.set(index, value + "");
-                    numArr.remove(index + 1);
-                    operationArr.remove(index);
-                }
-                else{
-                    int index = operationArr.indexOf("-");
-                    double value = Double.parseDouble(numArr.get(index)) - Double.parseDouble(numArr.get(index+1));
-                    numArr.set(index, value + "");
-                    numArr.remove(index + 1);
-                    operationArr.remove(index);
-                }
+            if(decimals > 1){
+                two_decimals = true;
             }
-            view = numArr.get(0);
+        }
+        if(numArr.size() - operationArr.size() == 1 && !numArr.contains("") && !two_decimals){
+            while(operationArr.size() > 0) {
+                int index;
+                if (operationArr.contains("*")) {
+                    index = operationArr.indexOf("*");
+                    double value = Double.parseDouble(numArr.get(index)) * Double.parseDouble(numArr.get(index + 1));
+                    numArr.set(index, value + "");
+                } else if (operationArr.contains("/")) {
+                    index = operationArr.indexOf("/");
+                    double value = Double.parseDouble(numArr.get(index)) / Double.parseDouble(numArr.get(index + 1));
+                    numArr.set(index, value + "");
+                } else if (operationArr.contains("+")) {
+                    index = operationArr.indexOf("+");
+                    double value = Double.parseDouble(numArr.get(index)) + Double.parseDouble(numArr.get(index + 1));
+                    numArr.set(index, value + "");
+                } else {
+                    index = operationArr.indexOf("-");
+                    double value = Double.parseDouble(numArr.get(index)) - Double.parseDouble(numArr.get(index + 1));
+                    numArr.set(index, value + "");
+                }
+                numArr.remove(index + 1);
+                operationArr.remove(index);
+            }
+            Log.i("omkar", numArr.get(0));
+            if(numArr.get(0).equals("Infinity")){
+                view = "ERR";
+                screen.setText(view);
+                numArr.remove(0);
+            }
+            else {
+                currentnum = "" + numArr.get(0);
+                view = numArr.get(0);
+                numArr.remove(0);
+            }
+
         }
         else{
             view = "ERR";
             screen.setText(view);
+            numArr.clear();
+            operationArr.clear();
+            currentnum = "";
         }
 
 
+    }
+    //clears all data
+    public void clear(View V){
+        view = "";
+        screen.setText(view);
+        numArr.clear();
+        operationArr.clear();
+        currentnum = "";
+    }
+    //deletes the last character added
+
+    public void delete(View v){
+        if(view.length() > 0){
+            view = view.substring(0, view.length()-1);
+            screen.setText(view);
+        }
+        if(numIsLastChanged && currentnum.length() == 1){
+            currentnum = "";
+        }
+        else if(numIsLastChanged && currentnum.length() != 0){
+            currentnum = currentnum.substring(0, currentnum.length()-1);
+        }
+        else if(!numIsLastChanged){
+            operationArr.remove(operationArr.size()-1);
+            numArr.remove(numArr.size()-1);
+            currentnum = numArr.get(numArr.size()-1);
+        }
     }
 
 
